2361560581a9a6fffe5408f825f07ddedc4b4cc7
Reece Crook <reececrook16@gmail.com>
2026-03-02 11:26:38 -0700

    Added more pictures to the home page carousel || Fleshed out the Floorplans header look: Went with the pipe divider on desktop and new lines on mobile, fixed the main divider lines not lining up || Changed the mobile-circle background color to the correct light teal

--- src/Components/FloorPlans.js
+++ src/Components/FloorPlans.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../css/FloorPlan.css';
 import { floorPlanImages } from '../images';
 import ImageCarousel from './ImageCarousel';
+
 function FloorPlans() {
   return (
     <div className="floorPlansPage">
       <div className="floorPlanWrapper">
         <article className="floorPlanCard FPCard1">
           <div className="cardText">
-            <h2>2 Bedroom - 1 Bath - 2 Car Garage</h2>
-            <p>2 Car Garage</p>
+            <h2>2 Bedroom <span className="pipe">|</span> 1 Bath <span className="pipe">|</span> 2 Car Garage</h2>
           </div>
           <hr className="divider" />
           <ImageCarousel pics={floorPlanImages[0]} showFullscreenButton={true} />
@@ -17,23 +17,11 @@ function FloorPlans() {
 
         <article className="floorPlanCard FPCard2">
           <div className="cardText">
-            <h2>3 Bedroom | 3 Bath | 2 Car Garage</h2>
-          </div>
-          {/* <hr className="divider" /> */}
-          <ImageCarousel pics={floorPlanImages[1]} showFullscreenButton={true} />
-        </article>
-{/* 
-        <article className="floorPlanCard FPCard3">
-          <div className="cardText">
-            <h2>3 Bedroom 2.5 Bath</h2>
-            <p>
-              Unfinished Basement<br />
-              1 car detached garage
-            </p>
+            <h2>3 Bedroom <span className="pipe">|</span> 3 Bath <span className="pipe">|</span> 2 Car Garage</h2>
           </div>
           <hr className="divider" />
-          <ImageCarousel pics={floorPlanImages[]} showFullscreenButton={true} />
-        </article> */}
+          <ImageCarousel pics={floorPlanImages[1]} showFullscreenButton={true} />
+        </article>
       </div>
     </div>
   );
--- src/images.js
+++ src/images.js
@@ -10,16 +10,19 @@ import img8 from './assets/HomePagePics/Garage.jpeg'
 import img9 from './assets/HomePagePics/Outer.jpeg'
 import img10 from './assets/HomePagePics/OuterBack.jpeg'
 import img11 from './assets/HomePagePics/OuterBack2.jpeg'
-// import buyerIncentive from './assets/HomePagePics/firstTimeHomebuyer.png'
+import img12 from './assets/HomePagePics/Bedroom2.jpg'
+import img13 from './assets/HomePagePics/FrontView.jpg'
+import img14 from './assets/HomePagePics/Kitchen2.jpg'
+import img15 from './assets/HomePagePics/Main3.jpg'
+import img16 from './assets/HomePagePics/Main4.jpg'
+import img17 from './assets/HomePagePics/Bathroom2.jpg'
 
 
 import card1img1 from './assets/FloorPlans/card1/Unit 101 Floorplan.jpg'
 import card1img2 from './assets/FloorPlans/card1/Unit 101 Main Level.jpg'
-// import card1img3 from './assets/FloorPlans/card1/Model 1551L- Basement.jpg'
 
 import card2img1 from './assets/FloorPlans/card2/3bd_3ba Front Street Upper Level.jpg'
 import card2img2 from './assets/FloorPlans/card2/3bd_3ba Front Street Main Level.jpg'
-// import card2img3 from './assets/FloorPlans/card2/237-Front-St-Arch-Rev2-images-2.jpg'
 
 import card3img1 from './assets/FloorPlans/card3/Model 1558-main.jpg'
 import card3img2 from './assets/FloorPlans/card3/Model 1558-Upper.jpg'
@@ -32,68 +35,98 @@ const homeImages = [
         "thumbnail": img1,
         'originalClass': 'homeImgs',
     },
-    // {
-    //     'id': 4,
-    //     'original': buyerIncentive,
-    //     "thumbnail": buyerIncentive,
-    //     'originalClass': 'homeImgs'
-    // },
     {
-        'id': 3,
+        'id': 2,
         'original': img2,
         "thumbnail": img2,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 2,
+        'id': 3,
         'original': img3,
         "thumbnail": img3,
         'originalClass': 'homeImgs'
     },
     {
+        'id': 4,
+        'original': img15,
+        "thumbnail": img15,
+        'originalClass': 'homeImgs'
+    },
+    {
         'id': 5,
+        'original': img16,
+        "thumbnail": img16,
+        'originalClass': 'homeImgs'
+    },
+    {
+        'id': 6,
         'original': img4,
         "thumbnail": img4,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 6,
+        'id': 7,
+        'original': img14,
+        "thumbnail": img14,
+        'originalClass': 'homeImgs'
+    },
+    {
+        'id': 8,
         'original': img5,
         "thumbnail": img5,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 7,
+        'id': 9,
+        'original': img17,
+        "thumbnail": img17,
+        'originalClass': 'homeImgs'
+    },
+    {
+        'id': 10,
         'original': img6,
         "thumbnail": img6,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 8,
+        'id': 11,
+        'original': img12,
+        "thumbnail": img12,
+        'originalClass': 'homeImgs'
+    },
+    {
+        'id': 12,
         'original': img7,
         "thumbnail": img7,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 9,
+        'id': 13,
         'original': img8,
         "thumbnail": img8,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 10,
+        'id': 14,
+        'original': img13,
+        "thumbnail": img13,
+        'originalClass': 'homeImgs'
+    },
+    {
+        'id': 15,
         'original': img9,
         "thumbnail": img9,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 11,
+        'id': 16,
         'original': img10,
         "thumbnail": img10,
         'originalClass': 'homeImgs'
     },
     {
-        'id': 12,
+        'id': 17,
         'original': img11,
         "thumbnail": img11,
         'originalClass': 'homeImgs'
@@ -114,13 +147,7 @@ const floorPlanImages = [
             "thumbnail": card1img2,
             'model': 1551,
             'originalClass': 'cardImgs'
-        },
-        // {
-        //     'original': card1img3,
-        //     "thumbnail": card1img3,
-        //     'model': 1551,
-        //     'originalClass': 'cardImgs'
-        // }
+        }
     ],
     [
         {
@@ -134,13 +161,7 @@ const floorPlanImages = [
             "thumbnail": card2img2,
             'model': 1551,
             'originalClass': 'cardImgs'
-        },
-        // {
-        //     'original': card2img3,
-        //     "thumbnail": card2img3,
-        //     'model': 1551,
-        //     'originalClass': 'cardImgs'
-        // }
+        }
     ],
     [
         {
